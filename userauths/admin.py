62007bb7390a9f36fb988a0a17d272c7cc3e2d93
diddyjax19 <adetobi59@gmail.com>
2023-03-12 15:51:22 +0000

    changes made to the admin.py file of the userauths folder

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from userauths.models import User, ContactUs, Profile
 
-# Register your models here.
+# render admin look for user model
+class UserAdmin(admin.ModelAdmin):
+    # set list display to control which fields are displayed on the change_list page of the admin
+    list_display = ['username', 'email', 'bio']
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'subject']
+
+# imports an admin module in each installed app. Registers model within each admin
+admin.site.register(User, UserAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
+admin.site.register(Profile)
