607c5e03fc76d7a43c66c1b2ea4a3fb27d717ead
diddyjax19 <adetobi59@gmail.com>
2023-03-12 16:05:12 +0000

    changes made to the admin.py file in the core app

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from core.models import CartOrderProducts, Product, Category, Vendor, CartOrder, ProductImages, ProductReview, wishlist_model, Address
 
-# Register your models here.
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin]
+    list_editable = ['title', 'price', 'featured', 'product_status']
+    list_display = ['user', 'title', 'product_image', 'price', 'category', 'vendor', 'featured', 'product_status', 'pid']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category_image']
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'vendor_image']
+
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_editable = ['paid_status', 'product_status', 'sku']
+    list_display = ['user',  'price', 'paid_status', 'order_date','product_status', 'sku']
+
+
+class CartOrderProductsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'invoice_no', 'item', 'image','qty', 'price', 'total']
+
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'review', 'rating']
+
+
+class wishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_editable = ['address', 'status']
+    list_display = ['user', 'address', 'status']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderProducts, CartOrderProductsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(wishlist_model, wishlistAdmin)
+admin.site.register(Address, AddressAdmin)
