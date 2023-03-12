aa8526af6bacfe81e77bef9e7a7ebfe9500cc9e3
diddyjax19 <adetobi59@gmail.com>
2023-03-12 15:44:37 +0000

    create a form.py file in the useradmin app

---
+++ useradmin/forms.py
@@ -0,0 +1,41 @@
+from core.models import Product
+from django import forms
+# from bootstrap_datepicker_plus import DatePickerInput
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
+
+
+
+class AddProductForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Product Title", "class":"form-control"}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "Product Description", "class":"form-control"}))
+    price = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': "Sale Price", "class":"form-control"}))
+    old_price = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': "Old Price", "class":"form-control"}))
+    type = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Type of product e.g organic cream", "class":"form-control"}))
+    stock_count = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': "How many are in stock?", "class":"form-control"}))
+    life = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "How long would this product live?", "class":"form-control"}))
+    mfd = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'placeholder': "e.g: 22-11-02", "class":"form-control"}))
+    tags = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Tags", "class":"form-control"}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={"class":"form-control"}))
+
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'image',
+            'description',
+            'price',
+            'old_price',
+            'specifications',
+            'type',
+            'stock_count',
+            'life',
+            'mfd',
+            'tags',
+            'digital',
+            'category',
+        ]
+
+        widgets = {
+        'mdf': DateTimePickerInput
+    }
+        
