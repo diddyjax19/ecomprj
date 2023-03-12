dc445935727b13ec3eefe7740ee009632aa49af3
diddyjax19 <adetobi59@gmail.com>
2023-03-12 15:56:34 +0000

    changes made to the urls.py  file of the userauths folder

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,63 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+# Create custom user model by extending Abstract user
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    # returns a human-readable, or informal, string representation of an object
+    def __str__(self):
+        return self.username
+
+
+# Profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="image")
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200) # +234 (456) - 789
+    verified = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.full_name} - {self.bio}"
+
+
+# Contact us model
+class ContactUs(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200) # +234 (456) - 789
+    subject = models.CharField(max_length=200) # +234 (456) - 789
+    message = models.TextField()
+
+# Inner class of the model class, used to change the behaviour of model fields like verbose_name
+    class Meta:
+        verbose_name = "Contact Us"
+        verbose_name_plural = "Contact Us"
+
+    def __str__(self):
+        return self.full_name
+
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+#  helps us connect events with actions
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)    
+
+
+
