4e9f6e88f0fc31a1dd6f4622555ab7bf25fe5832
diddyjax19 <adetobi59@gmail.com>
2023-03-13 08:55:14 +0000

    changes made in settings.py folder and migration made

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -212,7 +212,3 @@ PAYPAL_RECEIVER_EMAIL = 'nestifystore@gmail.com'
 PAYPAL_TEST = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://nextit12.herokuapp.com/'
-]
